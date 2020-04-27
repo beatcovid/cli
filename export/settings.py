de5895606f2229ddffeeac8993e93fd8fb41a069
Nik Cubrilovic <nc9@protonmail.com>
2020-04-27 12:19:11 +1000

    Remove default credentials so we can eventually open this repo up

--- export/settings.py
+++ export/settings.py
@@ -1,6 +1,5 @@
 import os
 
-
 """
 The ACCESS_KEY SECRET_KEY are credentials AWS credentials with write privileges
 to BUCKET_NAME.
@@ -8,18 +7,14 @@ to BUCKET_NAME.
 FORM_ID is a number id of the kobo form to export data from, hosted at
 KOBOCAT_API_URI with authorization provided by KOBOCAT_API_CREDENTIALS
 """
-ACCESS_KEY = os.environ.get("ACCESS_KEY", "unknown")
-SECRET_KEY = os.environ.get("SECRET_KEY", "unknown")
-BUCKET_NAME = os.environ.get("S3_BUCKET", default="aws455-apse2-covid19-prod-export")
+ACCESS_KEY = os.environ.get("ACCESS_KEY", default=None)
+SECRET_KEY = os.environ.get("SECRET_KEY", default=None)
+BUCKET_NAME = os.environ.get("S3_BUCKET", default=None)
 
 FORM_ID = os.environ.get("FORM_ID", "3")
 
-KOBOCAT_API_URI = os.environ.get(
-    "KOBOCAT_API", default="https://kc.beatcovid19now.org/"
-)
+KOBOCAT_API_URI = os.environ.get("KOBOCAT_API", default="https://kc.beatcovid19now.org/")
 
-KOBOCAT_API_CREDENTIALS = os.environ.get(
-    "KOBOCAT_CREDENTIALS", default="c3VwZXJfYWRtaW46WkxOJEM3WTh6WA=="
-)
+KOBOCAT_API_CREDENTIALS = os.environ.get("KOBOCAT_CREDENTIALS", default=None)
 
 REDIS_HOST = os.environ.get("REDIS_HOST", default="127.0.0.10")
