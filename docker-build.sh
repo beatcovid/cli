762ea24e3a9182d2d0211d79911531d9b368bb0d
Nik Cubrilovic <nc9@protonmail.com>
2020-04-29 08:18:05 +1000

    remove docker files

--- docker-build.sh
+++
@@ -1,4 +0,0 @@
-# test docker build locally
-
-docker build -t beatcovid/export .
-docker run --rm -it beatcovid/export
--- docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-# activate venv
-. .venv/bin/activate
-
-exec "$@"
